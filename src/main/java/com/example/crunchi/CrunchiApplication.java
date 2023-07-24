b967a657a98c0ea7c341a789e72262f3e80a1553
matias1364 <matiasleoncontreras@gmail.com>
2023-07-24 19:12:09 -0300

    frst commit

---
+++ src/main/java/com/example/crunchi/CrunchiApplication.java
@@ -0,0 +1,13 @@
+package com.example.crunchi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrunchiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrunchiApplication.class, args);
+	}
+
+}
