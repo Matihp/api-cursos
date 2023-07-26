2bd687f5e3db1d7cfb897909abf4ea936bbbcd9a
matias1364 <matiasleoncontreras@gmail.com>
2023-07-26 19:04:57 -0300

    delete project

--- src/main/java/com/example/crunchi/repository/AnimeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.crunchi.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.example.crunchi.entity.Anime;
-
-public interface AnimeRepository extends CrudRepository<Anime, Long> {
-
-}
